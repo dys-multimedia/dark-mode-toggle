be3db89c46af11b5dd7be51bfe7fc0f1395d93b8
David Soards <davidysoards@gmail.com>
2020-02-07 00:38:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Logo from './Logo';
+import Toggle from './Toggle';
+
+const lightTheme = {
+  '--color-bg': '#ffffff',
+  '--color-text': '#303846',
+  '--color-primary': '#fc7e0f',
+};
+
+const darkTheme = {
+  '--color-bg': '#303846',
+  '--color-text': '#ffffff',
+  '--color-primary': '#09d3ac',
+};
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const currentTheme = darkMode ? darkTheme : lightTheme;
+    Object.entries(currentTheme).forEach(entry => {
+      document.documentElement.style.setProperty(entry[0], entry[1]);
+    });
+  }, [darkMode]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
       </header>
+      <main className="main">
+        <div className="wrapper">
+          <h1>Dark Mode Toggle</h1>
+          <Logo className="logo" />
+          <p>
+            Make the buttons and toggle in the header switch between light and
+            dark mode.
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
